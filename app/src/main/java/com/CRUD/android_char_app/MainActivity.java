673fbabf5fa5eb083e799636cf85db7d6ef0d335
kunal <kunalzadepc@outlook.com>
2022-10-21 23:42:03 +0530

    update

--- app/src/main/java/com/CRUD/android_char_app/MainActivity.java
+++ app/src/main/java/com/CRUD/android_char_app/MainActivity.java
@@ -2,12 +2,28 @@ package com.CRUD.android_char_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button check;
+    EditText email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        check=findViewById(R.id.button);
+        email=findViewById(R.id.editTextTextEmailAddress);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "IT's working",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
